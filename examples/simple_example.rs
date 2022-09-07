34e4a117d29c0a1edf5e7a3993e11b5efde5c8ed
expploitt <moisescarralortiz@gmail.com>
2022-09-07 11:00:02 +0200

    Detected devices

---
+++ examples/simple_example.rs
@@ -0,0 +1,20 @@
+
+
+use ipsp_plugin::IpspManager;
+use async_ctrlc::CtrlC;
+use futures::StreamExt;
+
+#[async_std::main]
+async fn main() {
+
+    env_logger::init();
+    let ipsp = IpspManager::new("");
+
+    let (a, handle) = ipsp.start().await;
+
+    let signal = CtrlC::new().expect("Error creating CTRL+C signal handler");
+    let mut _ctrl_c = signal.enumerate().take(1);
+
+    _ctrl_c.next().await;
+    ipsp.stop().await;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
+use bluez_async::DiscoveryFilter;
+
+mod device;
+
+const SCANNING_SLOT: Duration = Duration::from_secs(5);
+
+#[derive(Clone)]
+pub struct IpspManager {
+    name_filter: Option<String>,
+    devices: Vec<device::Device>,
+    flag: Arc<AtomicBool>,
 }
+
+impl Drop for IpspManager {
+    fn drop(&mut self) {
+        async_std::task::block_on(self.stop());
+    }
+}
+
+impl Default for IpspManager {
+    fn default() -> Self {
+        Self {
+            name_filter: Some(String::from("GTI IPSP")),
+            devices: vec![],
+            flag: Arc::new(AtomicBool::new(true)),
+        }
+    }
+}
+
+impl IpspManager {
+    pub fn new(filter: Option<String>) -> Self {
+        Self {
+            name_filter: filter,
+            devices: vec![],
+            flag: Arc::new(AtomicBool::new(true)),
+        }
+    }
+
+    pub async fn start(&self) -> (async_std::channel::Sender<()>, async_std::task::JoinHandle<()>) {
+        let (sender, receiver) = async_std::channel::bounded(1);
+
+        let ipsp = self.clone();
+        let handle = async_std::task::spawn_blocking(move || {
+            async_std::task::block_on(async { ipsp.run(receiver).await })
+        });
+
+        (sender, handle)
+    }
+
+    pub async fn stop(&self) {
+        self.flag.store(false, Ordering::Relaxed);
+    }
+
+    async fn run(&self, recv: async_std::channel::Receiver<()>) {
+        println!("IPSP Plugin running...");
+        /* SETUP 6LOWPAN CONFIG*/
+
+        /* INIT SESSION*/
+        let (bluez_handle, session) = bluez_async::BluetoothSession::new().await.unwrap();
+
+
+        while self.flag.load(Ordering::Relaxed) {
+            /* DISCOVER PHASE */
+            let mut filter = DiscoveryFilter::default();
+            filter.pattern = self.name_filter.clone();
+
+            session.start_discovery_with_filter(&filter).await.unwrap();
+            async_std::task::sleep(SCANNING_SLOT).await;
+            session.stop_discovery().await.unwrap();
+
+            if let Ok(devices) = session.get_devices().await {
+                for device in devices {
+                    if !device.connected {
+                        println!("Discovered device: {:?}", device);
+                        /* TRY TO CONNECT */
+                    }
+                }
+            } else {
+                println!("Devices not discovered!");
+            }
+            /* ADD NEW TASK FOR NEW DEVICE */
+        }
+    }
+
+    pub async fn discover(&self) {}
+}
+
