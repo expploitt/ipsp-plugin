4ea5eb2e174d478991826b514309a0609dbb2b1a
expploitt <moisescarralortiz@gmail.com>
2022-09-07 12:12:08 +0200

    Added connect command

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::process::Command;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
-use bluez_async::DiscoveryFilter;
+use bluez_async::{BluetoothSession, DiscoveryFilter};
 
 mod device;
+mod macros;
 
 const SCANNING_SLOT: Duration = Duration::from_secs(5);
 
@@ -63,19 +65,28 @@ impl IpspManager {
 
 
         while self.flag.load(Ordering::Relaxed) {
-            /* DISCOVER PHASE */
-            let mut filter = DiscoveryFilter::default();
-            filter.pattern = self.name_filter.clone();
-
-            session.start_discovery_with_filter(&filter).await.unwrap();
-            async_std::task::sleep(SCANNING_SLOT).await;
-            session.stop_discovery().await.unwrap();
+            self.discover(&session).await;
 
             if let Ok(devices) = session.get_devices().await {
                 for device in devices {
                     if !device.connected {
                         println!("Discovered device: {:?}", device);
                         /* TRY TO CONNECT */
+                        let mac = &device.mac_address.to_string();
+                        let _type = 2;
+
+                        let command = connect_command!(mac, _type);
+                        let mut command = Command::new(command)
+                            .spawn()
+                            .expect("Error connecting device");
+
+                        command.wait().unwrap();
+
+                        let mut addr_command = Command::new("ip address add 2001:db8::2/64 dev bt0")
+                            .spawn()
+                            .expect("Error configuring ip address");
+
+                        addr_command.wait().unwrap();
                     }
                 }
             } else {
@@ -85,6 +96,13 @@ impl IpspManager {
         }
     }
 
-    pub async fn discover(&self) {}
+    pub async fn discover(&self, session: &BluetoothSession) {
+        let mut filter = DiscoveryFilter::default();
+        filter.pattern = self.name_filter.clone();
+
+        session.start_discovery_with_filter(&filter).await.unwrap();
+        async_std::task::sleep(SCANNING_SLOT).await;
+        session.stop_discovery().await.unwrap();
+    }
 }
 
